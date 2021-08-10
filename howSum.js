293a8b009e691be72c11d5e9b0a6494034613715
uglydavy <aknk.v@protonmail.ch>
2021-08-10 20:06:23 +0800

    create and updated howSum.js

---
+++ howSum.js
@@ -0,0 +1,18 @@
+const howSum = ( target, box, memo = {} ) => {
+    if (target in memo) return memo[target]
+    if (target === 0) return []
+    if (target < 0) return null
+    for (let i of box) {
+        const remain = target - i
+        const res = howSum(remain, box)
+        if (res !== null) {
+            memo[target] = [...res, i]
+            return memo[target]
+        }
+    }
+    memo[target] = null
+    return memo[target]
+}
+
+console.log( howSum(7, [5, 3, 4, 7]) )
+console.log( howSum(300, [7, 4]) )
